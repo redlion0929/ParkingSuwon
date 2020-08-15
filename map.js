523379e8116a2eae8a08e0af338670f4dcae7efe
Seung Won Jung <redlion0929@naver.com>
2020-08-15 16:47:31 +0900

    media query editt

--- map.js
+++ map.js
@@ -1,7 +1,11 @@
 var mapOptions= {
         center:new naver.maps.LatLng(37.266444,126.997219), //초기위치
         zoom:15,
-    };
+        zoomControl:true,
+        zoomControlOptions: {
+            position: naver.maps.Position.LEFT_BOTTOM
+        }
+        };
 
 function showMarker(lat,lng) {
 var marker = new naver.maps.Marker({
